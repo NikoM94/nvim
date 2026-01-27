e2381129dabb53538aa76c8a9af18bb341871323
Niko Muukkonen <niko.muukkonen@edu.keuda.fi>
2026-01-27 23:07:13 +0200

    use config function instead of opts

--- lua/custom/plugins/everviolet.lua
+++ lua/custom/plugins/everviolet.lua
@@ -2,21 +2,24 @@ return {
   'everviolet/nvim',
   name = 'evergarden',
   priority = 1000, -- Colorscheme plugin is loaded first before any other plugins
-  opts = {
-    theme = {
-      variant = 'fall', -- 'winter'|'fall'|'spring'|'summer'
-      accent = 'green',
-    },
-    editor = {
-      transparent_background = false,
-      sign = { color = 'none' },
-      float = {
-        color = 'mantle',
-        solid_border = false,
+  config = function()
+    require('evergarden').setup {
+      theme = {
+        variant = 'winter', -- 'winter'|'fall'|'spring'|'summer'
+        accent = 'green',
       },
-      completion = {
-        color = 'surface0',
+      editor = {
+        transparent_background = false,
+        sign = { color = 'none' },
+        float = {
+          color = 'mantle',
+          solid_border = false,
+        },
+        completion = {
+          color = 'surface0',
+        },
       },
-    },
-  },
+    }
+    vim.cmd.colorscheme 'evergarden'
+  end,
 }
